def93dbd746ea2402f64a9868eaf01405fb55df6
JuusoPesonen <c2peju00@students.oamk.fi>
2022-12-04 14:20:37 +0200

    Added js for current page hover effect

---
+++ Javascript/main.js
@@ -0,0 +1,7 @@
+//Current page location hover
+
+document.querySelectorAll('.nav-link').forEach(link => {
+    if(link.href === window.location.href) {
+        link.setAttribute('aria-current', 'page')
+    }
+})
